8fb291ee32fe1aac7dce564b362cd855739b0ef4
Luigi Arrieta <luigi10arrieta@gmail.com>
2022-12-29 23:19:05 -0500

    Template Project

---
+++ src/main/java/com/kometsalestest/flowerinventory/FlowerinventoryApplication.java
@@ -0,0 +1,13 @@
+package com.kometsalestest.flowerinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerinventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerinventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kometsalestest/flowerinventory/FlowerinventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kometsalestest.flowerinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerinventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
